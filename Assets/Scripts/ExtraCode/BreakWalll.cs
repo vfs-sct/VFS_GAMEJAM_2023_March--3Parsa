caf8b1914d78f932e510e2ba8da70a4f56c8760a
CO <cforquiola@gmail.com>
2023-03-11 00:30:22 -0800

    Adjustments
    Update:
     - Preparing for tomorrow
      - Prepared breakable wall
      - Adjusted UI
        - In-game & pause

--- Assets/Scripts/ExtraCode/BreakWalll.cs
+++ Assets/Scripts/ExtraCode/BreakWalll.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Timer : MonoBehaviour
+public class BreakWalll : MonoBehaviour
 {
-    
+    // Needs hp
 }
---
+++ Assets/Scripts/ExtraCode/UI/Timer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float currentTime = 0f;
+    public float startingTime = 270f;   // 4 Minutes
+
+    [SerializeField] Text countdownText;
+
+    public int scene;   // for scene... Remove when isAlive is present
+
+    void Start()
+    {
+        currentTime = startingTime;
+        countdownText = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = "Time: " + currentTime.ToString("0");
+
+        if (currentTime <= 3)
+        {
+            countdownText.color = Color.red;
+        }
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            countdownText.color = Color.gray;
+
+            SceneManager.LoadScene(scene);  // replace later
+
+            print("No more time.");
+        }
+    }
+}
